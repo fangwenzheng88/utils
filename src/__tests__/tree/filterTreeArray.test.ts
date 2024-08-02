df0677b53e9e271a99358e63a70ea676f44cd4d3
fangwenzheng <fangwenzheng@cestc.cn>
2024-08-02 16:17:04 +0800

    feat: 新增filterTreeArray2/filterTreeArray3两个函数

--- src/__tests__/tree/filterTreeArray.test.ts
+++ src/__tests__/tree/filterTreeArray.test.ts
@@ -1,5 +1,5 @@
 import { test, expect, describe } from 'vitest'
-import { filterTreeArray } from '../../tree'
+import { filterTreeArray, filterTreeArray2, filterTreeArray3 } from '../../tree'
 
 const treeDataArr = [
   {
@@ -87,6 +87,113 @@ describe('filterTreeArray', () => {
 
   test('所有都不满足，返回空数组', () => {
     const result = filterTreeArray(treeDataArr, () => {
+      return false
+    })
+    expect(result).toEqual([])
+  })
+})
+
+describe('filterTreeArray2', () => {
+  test('子节点满足，父节点将会被保留', () => {
+    const result = filterTreeArray2(treeDataArr, (node) => {
+      return node.key === '1-2-1'
+    })
+    expect(result).toEqual([
+      {
+        key: '1',
+        children: [
+          {
+            key: '1-2',
+            children: [
+              {
+                key: '1-2-1',
+              },
+            ],
+          },
+        ],
+      },
+    ])
+  })
+
+  test('父节点满足，子节点不满足将会被过滤掉', () => {
+    const result = filterTreeArray2(treeDataArr, (node) => {
+      return node.key === '1'
+    })
+    expect(result).toEqual([
+      {
+        key: '1',
+        children: [],
+      },
+    ])
+  })
+
+  test('保留满足条件的节点和其父节点', () => {
+    const result = filterTreeArray2(treeDataArr, (node) => {
+      return ['1', '2-1-1'].includes(node.key)
+    })
+    expect(result).toEqual([
+      {
+        key: '1',
+        children: [],
+      },
+      {
+        key: '2',
+        children: [
+          {
+            key: '2-1',
+            children: [
+              {
+                key: '2-1-1',
+              },
+            ],
+          },
+        ],
+      },
+    ])
+  })
+
+  test('所有都不满足，返回空数组', () => {
+    const result = filterTreeArray2(treeDataArr, () => {
+      return false
+    })
+    expect(result).toEqual([])
+  })
+})
+
+describe('filterTreeArray3', () => {
+  test('子节点满足，父节点不满足，不保留', () => {
+    const result = filterTreeArray3(treeDataArr, (node) => {
+      return node.key === '1-2-1'
+    })
+    expect(result).toEqual([])
+  })
+
+  test('父节点满足，子节点不满足被过滤掉', () => {
+    const result = filterTreeArray3(treeDataArr, (node) => {
+      return node.key === '1'
+    })
+    expect(result).toEqual([
+      {
+        key: '1',
+        children: [],
+      },
+    ])
+  })
+
+  test('孙节点满足，父节点不满足不保留', () => {
+    const result = filterTreeArray3(treeDataArr, (node) => {
+      return ['2', '2-1-1'].includes(node.key)
+    })
+    expect(result).toEqual([
+      {
+        key: '2',
+        children: [],
+      },
+    ])
+  })
+
+  test('所有都不满足，返回空数组', () => {
+    const result = filterTreeArray3(treeDataArr, () => {
       return false
     })
     expect(result).toEqual([])
--- src/tree/filterTreeArray.ts
+++ src/tree/filterTreeArray.ts
@@ -2,9 +2,10 @@ import type { TreeNode } from './types'
 
 /**
  * 递归过滤树形结构数据
- * - 返回的数据是对treeDataArr数组中每一项的浅拷贝，不会修改原始treeDataArr中的值
- * - node满足条件，子节点将不会被过滤，所有子节点将会保留
+ *
+ * - 节点满足条件，子节点将不会被过滤，所有后代节点将会保留
  * - 子节点满足条件，父节点将会被保持留（维持树形结构）
+ * - 返回的数据是对treeDataArr数组中每一项的浅拷贝，不会修改原始treeDataArr中的值
  *
  * @category tree
  * @param treeDataArr 树形结构数据数组
@@ -30,10 +31,8 @@ import type { TreeNode } from './types'
 export function filterTreeArray<T extends TreeNode>(treeDataArr: readonly T[], filterFn: (node: T) => boolean, childrenFieldName: keyof T = 'children'): T[] {
   const filteredNodes: T[] = []
   for (const node of treeDataArr) {
-    const remainingNode = { ...node }
-
     if (filterFn(node)) {
-      const filteredNode: T = { ...remainingNode }
+      const filteredNode: T = { ...node }
       filteredNodes.push(filteredNode)
     } else {
       const children = node[childrenFieldName]
@@ -41,13 +40,101 @@ export function filterTreeArray<T extends TreeNode>(treeDataArr: readonly T[], f
         const filteredChildren = filterTreeArray(children, filterFn, childrenFieldName)
 
         if (filteredChildren.length > 0) {
-          const remainingNodeWithFilteredChildren = {
-            ...remainingNode,
+          filteredNodes.push({
+            ...node,
             [childrenFieldName]: filteredChildren,
-          }
-          filteredNodes.push(remainingNodeWithFilteredChildren)
+          })
         }
       }
+    }
+  }
+  return filteredNodes
+}
+
+/**
+ * 过滤树形数据数组中的节点
+ *
+ * - 父节点满足条件，将继续过滤子节点
+ * - 子节点满足条件，父节点将会被保持留（维持树形结构）
+ * - 返回的数据是对treeDataArr数组中每一项的浅拷贝，不会修改原始treeDataArr中的值
+ *
+ * @param treeDataArr 树形数据数组，包含多个树节点
+ * @param filterFn 过滤函数，用于判断节点是否符合条件，返回布尔值
+ * @param childrenFieldName 子节点数组的字段名，默认为 'children'
+ * @returns 返回一个新的，被过滤后的树形结构数组
+ *
+ * @example
+ * ```ts
+ * const treeDataArr = [
+ *   {key: '1',children: [{key: '1-1'},{key: '1-2',children: [{key: '1-2-1',},{key: '1-2-2'}]}]},
+ *   {key: '2',children: [{key: '2-1',children: [{key: '2-1-1'}]}]}
+ * ]
+ *
+ * filterTreeArray(treeDataArr, (node) => {
+ *   return node.key === '1-2'
+ * })
+ *
+ * 输出：
+ * [{key: '1',children: [{key: '1-2',children: []}]}]
+ * ```
+ */
+export function filterTreeArray2<T extends TreeNode>(treeDataArr: readonly T[], filterFn: (node: T) => boolean, childrenFieldName: keyof T = 'children'): T[] {
+  const filteredNodes: T[] = []
+  for (const node of treeDataArr) {
+    const filteredNode: T = { ...node }
+    const children = node[childrenFieldName]
+    let filteredChildren: T[] = []
+    if (Array.isArray(children) && children.length > 0) {
+      filteredChildren = filterTreeArray2(children, filterFn, childrenFieldName)
+      // @ts-ignore
+      filteredNode[childrenFieldName] = filteredChildren
+    }
+    if (filteredChildren.length > 0 || filterFn(node)) {
+      filteredNodes.push(filteredNode)
+    }
+  }
+  return filteredNodes
+}
+
+/**
+ * 过滤树形数据数组中的节点
+ *
+ * - 父节点和子节点都必须满足条件
+ * - 返回的数据是对treeDataArr数组中每一项的浅拷贝，不会修改原始treeDataArr中的值
+ *
+ * @param treeDataArr 树形数据数组，包含多个树节点
+ * @param filterFn 过滤函数，用于判断节点是否符合条件，返回布尔值
+ * @param childrenFieldName 子节点数组的字段名，默认为'children'
+ * @returns 返回一个新的，被过滤后的树形结构数组
+ *
+ * @example
+ * ```ts
+ * const treeDataArr = [
+ *   {key: '1',children: [{key: '1-1'},{key: '1-2',children: [{key: '1-2-1',},{key: '1-2-2'}]}]},
+ *   {key: '2',children: [{key: '2-1',children: [{key: '2-1-1'}]}]}
+ * ]
+ *
+ * filterTreeArray(treeDataArr, (node) => {
+ *   return node.key === '1-2'
+ * })
+ *
+ * 输出：
+ * []
+ * ```
+ */
+export function filterTreeArray3<T extends TreeNode>(treeDataArr: readonly T[], filterFn: (node: T) => boolean, childrenFieldName: keyof T = 'children'): T[] {
+  const filteredNodes: T[] = []
+  for (const node of treeDataArr) {
+    if (filterFn(node)) {
+      const filteredNode = { ...node }
+      const children = node[childrenFieldName]
+      if (Array.isArray(children) && children.length > 0) {
+        const filteredChildren = filterTreeArray3(children, filterFn, childrenFieldName)
+        // @ts-ignore
+        filteredNode[childrenFieldName] = filteredChildren
+      }
+
+      filteredNodes.push(filteredNode)
     }
   }
   return filteredNodes
